6cac3f57dbf4962a87b4d6600ba011586ff5994d
Gulshan kumar <210281116+Gulshankartikk@users.noreply.github.com>
2026-04-07 19:44:23 +0530

    Refactor code structure for improved readability and maintainability

--- 20lecture/02_anagram.cpp
+++ 20lecture/02_anagram.cpp
@@ -3,5 +3,35 @@
 using namespace std;
 int main()
 {
+    string s1, s2;
+    cout << "Enter first string: ";
+    cin >> s1;
+    cout << "Enter second string: ";
+    cin >> s2;
+
+    if (s1.size() != s2.size()) {
+        cout << "Not anagrams (different lengths)." << endl;
+        return 0;
+    }
+
+    int freq[256] = {0}; // Assuming ASCII character set
+
+    for (char c : s1) {
+        freq[c]++;
+    }
+
+    for (char c : s2) {
+        freq[c]--;
+    }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] != 0) {
+            cout << "Not anagrams." << endl;
+            return 0;
+        }
+    }
+
+    cout << "The strings are anagrams!" << endl;
+    return 0;
     
 }
