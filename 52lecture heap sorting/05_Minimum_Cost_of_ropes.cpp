ed878f6cdc5f7950b7c6f9630a586b9536897c6b
Gulshan kumar <210281116+Gulshankartikk@users.noreply.github.com>
2026-04-19 20:31:05 +0530

    Minimum Cost of ropes

---
+++ 52lecture heap sorting/05_Minimum_Cost_of_ropes.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        int cost =0;
+        priority_queue<int,vector<int>, greater<int> >p;
+        for(int i=0;i<arr.size();i++)
+            p.push(arr[i]);
+        
+        int first ,second;
+        while(p.size()!=1)
+        {
+            first =p.top();
+            p.pop();
+            second =p.top();
+            p.pop();
+            first+=second;
+            cost+=first;
+            p.push(first);
+        }
+        return cost;
+    }
+};
+
+int main() {
+    vector<int> arr = {4, 3, 2, 6};  // sample input
+    Solution obj;
+
+    int result = obj.minCost(arr);
+    cout << "Minimum Cost = " << result << endl;
+
+    return 0;
+}
