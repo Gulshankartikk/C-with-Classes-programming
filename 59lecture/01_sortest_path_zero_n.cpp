2c7a44b98609a694a804c5182188d4f7c79b2b36
Gulshan kumar <210281116+Gulshankartikk@users.noreply.github.com>
2026-07-04 21:01:12 +0530

    Add implementation for minimum steps calculation in a new file

---
+++ 59lecture/01_sortest_path_zero_n.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int minimumStep(int n) {
+    int count_edge = 0;
+
+    while (n >= 3) {
+        count_edge += n % 3;
+        n /= 3;
+        count_edge++;
+    }
+
+    count_edge += n;
+    count_edge--;
+
+    return count_edge;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << minimumStep(n) << endl;
+
+    return 0;
+}
