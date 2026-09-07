2e6e58cf8790e59f73806476cd242f96e0d09021
Gulshan kumar <210281116+Gulshankartikk@users.noreply.github.com>
2026-09-07 12:11:02 +0530

    floyd warshall

---
+++ 68lecture/01_Floyd_Warshall.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> dist(n, vector<int>(n));
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> dist[i][j];
+
+    for(int k = 0; k < n; k++)
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+            {
+                if(dist[i][k] != 100000000 && dist[k][j] != 100000000)
+                {
+                    dist[i][j] = min(dist[i][j],
+                                     dist[i][k] + dist[k][j]);
+                }
+            }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++)
+            cout << dist[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
